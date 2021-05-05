084c161df8f8041ea97f6815ca8107c45160500b
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-05-05 10:38:00 +0300

    Добавлен ActivityScope

--- android-dagger/app/src/main/java/com/example/android/dagger/registration/RegistrationComponent.kt
+++ android-dagger/app/src/main/java/com/example/android/dagger/registration/RegistrationComponent.kt
@@ -1,17 +1,17 @@
 package com.example.android.dagger.registration
 
-import android.content.Context
+import com.example.android.dagger.di.ActivityScope
 import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
 import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
-import dagger.BindsInstance
 import dagger.Subcomponent
 
+@ActivityScope
 @Subcomponent
 interface RegistrationComponent {
 
     @Subcomponent.Factory
     interface Factory {
-        fun create(@BindsInstance context: Context): RegistrationComponent
+        fun create(): RegistrationComponent
     }
 
     fun inject(activity: RegistrationActivity)
