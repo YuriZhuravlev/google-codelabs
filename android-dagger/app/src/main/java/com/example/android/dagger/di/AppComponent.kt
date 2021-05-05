f3bc2e1dfd71141ba7127901363ee59c42e10251
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-05-05 11:24:42 +0300

    Добавлен LoggedUserScope

--- android-dagger/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ android-dagger/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -2,9 +2,8 @@ package com.example.android.dagger.di
 
 import android.content.Context
 import com.example.android.dagger.login.LoginComponent
-import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
-import com.example.android.dagger.settings.SettingsActivity
+import com.example.android.dagger.user.UserManager
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -18,9 +17,8 @@ interface AppComponent {
         fun create(@BindsInstance context: Context): AppComponent
     }
 
+    fun userManager(): UserManager
+
     fun registrationComponent(): RegistrationComponent.Factory
     fun loginComponent(): LoginComponent.Factory
-
-    fun inject(activity: MainActivity)
-    fun inject(activity: SettingsActivity)
 }
